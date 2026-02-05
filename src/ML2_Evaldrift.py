4ed70b9fed516acd46111b8109355104313e530e
Gabriel101025 <S10259250@connect.np.edu.sg>
2026-02-05 21:22:38 +0800

    adjusted parameters for evaluation drift

--- src/ML2_Evaldrift.py
+++ src/ML2_Evaldrift.py
@@ -2,6 +2,7 @@ import pandas as pd
 import joblib
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from ML2_Asg_Pipeline import preprocess
+import numpy as np
 
 def main():
     model = joblib.load("model.pkl")
@@ -11,15 +12,17 @@ def main():
 
     # Evaluate on 2011
     X_2011 = df_2011.drop(columns=["cnt"])
-    y_2011 = df_2011["cnt"]
+    y_2011 = df_2011["cnt"].values
     preds_2011 = model.predict(X_2011)
-    rmse_2011 = mean_squared_error(y_2011, preds_2011, squared=False)
+    mse_2011 = mean_squared_error(y_2011, preds_2011)
+    rmse_2011 = np.sqrt(mse_2011)
 
     # Evaluate on 2012
     X_2012 = df_2012.drop(columns=["cnt"])
-    y_2012 = df_2012["cnt"]
+    y_2012 = df_2012["cnt"].values
     preds_2012 = model.predict(X_2012)
-    rmse_2012 = mean_squared_error(y_2012, preds_2012, squared=False)
+    mse_2012 = mean_squared_error(y_2012, preds_2012)
+    rmse_2012 = np.sqrt(mse_2012)
     #Compare
     print(f"2011 RMSE: {rmse_2011:.2f}")
     print(f"2012 RMSE: {rmse_2012:.2f}")
